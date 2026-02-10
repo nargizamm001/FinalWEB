3a01b017982490f3a91be0343d3b22ccf2292924
nargizamm001 <nargizamnaidarova@gmail.com>
2026-02-11 03:03:28 +0500

    last last

--- FinalWEB/server/src/services/emailService.js
+++ FinalWEB/server/src/services/emailService.js
@@ -3,11 +3,14 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: Number(process.env.SMTP_PORT),
-  secure: false,
+  secure: Number(process.env.SMTP_PORT) === 465,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
 });
 
 async function sendEmail({ subject, text }) {
