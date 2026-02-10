7630c40997d2fede789fae027ac3bfd3ed2bcf53
nargizamm001 <nargizamnaidarova@gmail.com>
2026-02-11 03:40:35 +0500

    .

--- FinalWEB/server/server.js
+++ FinalWEB/server/server.js
@@ -1,5 +1,6 @@
-const dotenv = require("dotenv");
-dotenv.config();
+console.log("BOOT OK - version 2026-02-11");
+
+require("dotenv").config();
 
 const app = require("./src/app");
 const { connectDB } = require("./src/config/db");
@@ -8,9 +9,11 @@ const PORT = process.env.PORT || 5000;
 
 connectDB()
   .then(() => {
-    app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+    app.listen(PORT, () => {
+      console.log("LISTENING", PORT);
+    });
   })
   .catch((err) => {
-    console.error("DB connection failed:", err);
+    console.error("DB connection failed:", err.message);
     process.exit(1);
   });
