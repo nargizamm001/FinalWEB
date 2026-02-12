4d176d4c6ecfc65e912a9edfe1f1871f568e165f
nargizamm001 <nargizamnaidarova@gmail.com>
2026-02-12 22:50:18 +0500

    .

--- FinalWEB/server/src/services/emailService.js
+++ FinalWEB/server/src/services/emailService.js
@@ -4,18 +4,25 @@ dns.setDefaultResultOrder("ipv4first");
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  host: "74.125.140.108", // IPv4 smtp.gmail.com
+  port: 587,
+  secure: false,
+  requireTLS: true,
   auth: {
     user: process.env.GMAIL_USER,
     pass: process.env.GMAIL_APP_PASSWORD,
   },
+  tls: {
+    servername: "smtp.gmail.com", // важно для TLS
+  },
+  connectionTimeout: 15000,
+  greetingTimeout: 15000,
+  socketTimeout: 20000,
 });
 
 async function sendEmail({ subject, text }) {
   return transporter.sendMail({
-    from: `"${process.env.EMAIL_FROM_NAME}" <${process.env.GMAIL_USER}>`,
+    from: `"${process.env.EMAIL_FROM_NAME || "App"}" <${process.env.GMAIL_USER}>`,
     to: process.env.ADMIN_EMAIL,
     subject,
     text,
